23e8a66c6262ec769c946c5048af3fd9cfb82b51
徐飞 <d.xuffy@qq.com>
2018-07-16 15:18:52 +0800

    修改

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -76,7 +76,9 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'),
+          resolve('node_modules/_element-ui@2.3.9@element-ui/src'),
+          resolve('node_modules/_element-ui@2.3.9@element-ui/packages')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
