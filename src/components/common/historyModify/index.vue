d5bbf93930eeabfe75ae390977b7764859de9429
liushuxiang <276900785@qq.com>
2018-07-19 15:29:35 +0800

    xx

--- src/components/common/historyModify/index.vue
+++ src/components/common/historyModify/index.vue
@@ -217,13 +217,11 @@
         this.showDialog = true;
         this.isModify = isModify;
 
-        if(this.$refs.filterColumn) {
           this.$nextTick(() => {
             this.$refs.filterColumn.update(false, dataList).then(res => {
               this.dataList = this.$refs.filterColumn.getFilterData(dataList, res);
             });
           })
-        }
         return dataList;
 
       },
