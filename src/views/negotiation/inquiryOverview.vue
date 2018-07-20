55d197ecff7c11764f23cb60603aaaa2e09c9c3f
dhc <dhc@ucnholding.com>
2018-07-20 15:23:14 +0800

    修复inquiry overview by sku 列显示问题

--- src/views/negotiation/inquiryOverview.vue
+++ src/views/negotiation/inquiryOverview.vue
@@ -46,7 +46,7 @@
       </div>
     </div>
     <v-table
-      code='inquiry_list'
+      :code="viewByStatus ? 'inquiry_sku_list': 'inquiry_list'"
       :data="tabData"
       :buttons="[{label: 'detail', type: 'detail'}]"
       :height="450"
