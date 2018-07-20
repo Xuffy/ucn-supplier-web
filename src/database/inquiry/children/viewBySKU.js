a5ecec6785c8f3b4ddbee27942121f7e9c83e853
dhc <dhc@ucnholding.com>
2018-07-20 14:32:51 +0800

    InquiryOverview显示inquiry date列

--- src/database/inquiry/children/viewBySKU.js
+++ src/database/inquiry/children/viewBySKU.js
@@ -11,6 +11,9 @@ export default {
     quotationNo: { // 询价子单号 系统自动生成
 
     },
+    entryDt: { // 创建时间
+        transForm: 'time'
+    },
     status: { // 系统带入，共四个状态及其触发： 询价状态
         transForm: 'INQUIRY_STATUS'
     },
