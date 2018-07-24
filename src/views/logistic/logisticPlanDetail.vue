c7bfe0005213c92ea5c84e2430ace90ad1c4b8f4
dengfukang <dengfukang@ucnholding.com>
2018-07-24 17:59:14 +0800

    no message

--- src/views/logistic/logisticPlanDetail.vue
+++ src/views/logistic/logisticPlanDetail.vue
@@ -272,22 +272,22 @@
       },
       productbButtons() {
         let aArr = [{
-            label: 'Negociate',
+            label: this.$i.logistic.Negociate,
             type: 1,
             disabled: !this.edit
           },
           {
-            label: 'Copy',
+            label:  this.$i.logistic.Copy,
             type: 4,
             disabled: !this.edit
           },
           {
-            label: 'Detail',
+            label:  this.$i.logistic.Detail,
             type: 3
           }
         ]
         this.$route.name == 'placeLogisticPlan' ? aArr : aArr.splice(1, 0, {
-          label: 'History',
+          label:  this.$i.logistic.History,
           type: 2,
           disabled: !this.edit
         })
@@ -617,6 +617,7 @@
           return this.$windowOpen({url:'/product/detail',params:{id:e.skuId.value}})
         } else if (status == 4) {
           let newAddArr = this.$depthClone(this.productList[i]);
+          console.log(newAddArr)
           newAddArr.id.value = null;
           newAddArr.fieldDisplay.value=null;
           newAddArr = _.mapObject(newAddArr,(v,k)=>{
