b07603d9d6711541ae0f293079c070612f3ecab5
liushuxiang <276900785@qq.com>
2018-08-01 14:13:07 +0800

    XX

--- src/views/order/orderDetail.vue
+++ src/views/order/orderDetail.vue
@@ -1521,7 +1521,7 @@
                         name: "已确认"
                     },
                     {
-                        code: "CANCLED",
+                        code: "CANCELED",
                         name: "已取消"
                     }
                 ];
