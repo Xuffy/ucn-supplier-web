78e139b0fe5a385ab95e8ab0903efdf2cc69e557
连超 <240896578@qq.com>
2018-08-08 16:36:25 +0800

    字段修改

--- src/database/warehouse/warehouseOverview.js
+++ src/database/warehouse/warehouseOverview.js
@@ -35,6 +35,7 @@ export default {
     innerCartonNetWeight:{},
     innerCartonGrossWeight:{},
     innerCartonVolume:{},
+    customerAbbr:{},
     // skuLength:{},
     // skuWidth:{},
     // skuHeight:{},
