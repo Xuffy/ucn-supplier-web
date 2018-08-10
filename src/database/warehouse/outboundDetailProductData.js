944b94377c0edd4e96b50401c8eaed13b3bb99f6
连超 <240896578@qq.com>
2018-08-10 10:47:29 +0800

    仓储天数

--- src/database/warehouse/outboundDetailProductData.js
+++ src/database/warehouse/outboundDetailProductData.js
@@ -15,6 +15,7 @@ export default {
   lengthUnitDictCode:{
     key:'lengthUnitDictCode',
   },
+  inventoryDays:{},
   exchangeCurrencyDictCode: {},
   volumeUnitDictCode:{},
   weightUnitDictCode:{},
