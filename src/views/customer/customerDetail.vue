dfdc2ae2952faf975ef66acd40542b5785a00ded
LAPTOP-Q5IGJGU8\admin <993826418@qq.com>
2018-08-22 13:59:53 +0800

    修改bug

--- src/views/customer/customerDetail.vue
+++ src/views/customer/customerDetail.vue
@@ -398,12 +398,28 @@
                       e.receiverAddress.value = e.receiveCountry._value+' '+receiveProvince+' '+receiveCity+' '+receiveAddress
                       return e;
                     });
-                    this.concats = this.$getDB(this.$db.supplier.supplierConcats, res.concats , e => {
-                      let gender;
-                      gender = _.findWhere(this.sex, {code: e.gender.value+''}) || {};
-                      e.gender._value = gender.name || '';
-                      return e;
-                    });
+                    let concats = res.concats
+                    //获取部门列表匹配
+                    if (this.basicDate.tenantId && this.$route.query.companyId){
+                      this.$ajax.get(`${this.$apis.GET_DEPARTMENT}?tenantId=${this.basicDate.tenantId}&companyId=${this.$route.query.companyId}`).then(res=>{
+                        console.log(1)
+                        this.concats = this.$getDB(this.$db.supplier.supplierConcats, concats, e => {
+                          let gender,deptId;
+                          gender = _.findWhere(this.sex, {code: (e.gender.value)+''}) || {};
+                          e.gender._value = gender.name || '';
+                          deptId = _.findWhere(res, {deptId: (e.deptId.value)}) || {};
+                          e.deptId._value = deptId.deptName || '';
+                          return e;
+                        });
+
+                      })
+                    }
+                    // this.concats = this.$getDB(this.$db.supplier.supplierConcats, res.concats , e => {
+                    //   let gender;
+                    //   gender = _.findWhere(this.sex, {code: e.gender.value+''}) || {};
+                    //   e.gender._value = gender.name || '';
+                    //   return e;
+                    // });
                     this.documentTypeClone = this.$depthClone(this.documentType)
                     this.documentTypeClone.forEach(v=>{
                       res.documents.forEach(m =>{
